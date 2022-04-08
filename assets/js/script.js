529abc6ca8abfff39ddecde52a22db1680473c23
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-04-09 01:11:59 +0530

    bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -192,7 +192,7 @@ window.addEventListener('load', () => {
 
 // Form validation
 
-const isRequired = (value) => value !== '';
+const isRequired = (value) => value === '';
 const isEmailValid = (email) => {
   const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return reg.test(email);
